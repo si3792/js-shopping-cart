ae20737837de4dd7ebe7ca2f9cb8486b189715f7
si3792 <simo.t.iliev@gmail.com>
2016-12-29 17:59:02 +0200

    Basic project structure

---
+++ app/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const DEBUG = true;
+
+const CONSTANTS = {
+  productTitleMin: 3,
+  productTitleMax: 250,
+  productDescMin: 5,
+  productDescMax: 500,
+  productImgMaxSizeMB: 3,
+  shippingTaxPercent: 5
+};
